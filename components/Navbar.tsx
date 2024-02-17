cc59421b2bfcd80ffac80dfeea2bf43b887a6ebb
bigdavecrypto69 <bigdavecryptooffical@gmail.com>
2024-02-17 00:37:34 +0000

    new update

---
+++ components/Navbar.tsx
@@ -0,0 +1,22 @@
+import { ConnectWallet, useAddress } from '@thirdweb-dev/react'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Navbar() {
+    const address = useAddress();
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.navbar}>
+                <div className={styles.navLinks}>
+                    {address && (
+                        <Link href={`profile/${address}`}>
+                            <p>My NFTs</p>
+                        </Link>
+                    )}
+                </div>
+                <ConnectWallet />
+            </div>
+        </div>
+    )
+}
